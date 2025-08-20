c4515f832353dd53439ddf8df10873b5975bd2e7
Yonatan Cale <myselfandfredy@gmail.com>
2025-08-19 22:19:02 -0700

    prompt folder: no dot prefix, because the obsidian viewer ignores those

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const CLAUDE_COPILOT_FOLDER = ".claude_copilot";
-export const CLAUDE_COPILOT_PROMPT_FILE = ".claude_copilot/prompt.md";
+export const CLAUDE_COPILOT_FOLDER = "claude_copilot";
+export const CLAUDE_COPILOT_PROMPT_FILE = `${CLAUDE_COPILOT_FOLDER}/prompt.md`;
